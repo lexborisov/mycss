ce577272a45c3f1522c1622b9ba337f7c135ad6e
Martijn Otto <martijn.otto@copernica.com>
2016-10-12 12:00:03 +0200

    Fixed Makefiles for correct include order of static libraries, fixed error about unused 'status' variable by actually checking the status in the examples and in the test.

--- examples/stylesheet_low_level.c
+++ examples/stylesheet_low_level.c
@@ -1,20 +1,20 @@
 /*
  Copyright (C) 2016 Alexander Borisov
- 
+
  This library is free software; you can redistribute it and/or
  modify it under the terms of the GNU Lesser General Public
  License as published by the Free Software Foundation; either
  version 2.1 of the License, or (at your option) any later version.
- 
+
  This library is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  Lesser General Public License for more details.
- 
+
  You should have received a copy of the GNU Lesser General Public
  License along with this library; if not, write to the Free Software
  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
- 
+
  Author: lex.borisov@gmail.com (Alexander Borisov)
 */
 
@@ -31,32 +31,35 @@ int main(int argc, const char * argv[])
 div > html|span:has(~ [class~=best i]) {width: 30%; height: 120.46px}\n\
 :not(h1, h2, h3, h4, h5) {}\n\
 body > div[id*=mu i]:not(:nth-child(2n+1)) {width: 1.4%}";
-    
+
     // basic init
     mycss_t *mycss = mycss_create();
     mycss_status_t status = mycss_init(mycss);
-    
+
+    // check initialization
+    if (MyCSS_FAILED(status)) return EXIT_FAILURE;
+
     // current entry work init
     mycss_entry_t *entry = mycss_entry_create();
     status = mycss_entry_init(mycss, entry);
-    
+
     // parse selectors
-    fprintf(stdout, "Input data:\n");
-    fprintf(stdout, "%s", css);
-    fprintf(stdout, "\n\n");
-    
+    printf("Input data:\n");
+    printf("%s", css);
+    printf("\n\n");
+
     status = mycss_parse(entry, MyHTML_ENCODING_UTF_8, css, strlen(css));
-    
+
     /* print result */
-    fprintf(stdout, "Result:\n");
+    printf("Result:\n");
     mycss_namespace_print_stylesheet(&entry->stylesheet->ns_stylesheet, stdout);
     mycss_stylesheet_print(entry->stylesheet, stdout);
-    fprintf(stdout, "\n");
-    
+    printf("\n");
+
     // destroy all
     mycss_entry_destroy(entry, true);
     mycss_destroy(mycss, true);
-    
+
     return 0;
 }
 
